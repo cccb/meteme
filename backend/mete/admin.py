3226a3514f3589b188e4e76fe2d0818532585051
Matthias Hannig <matthias@hannig.cc>
2016-04-15 10:30:51 +0200

    added transactions and transactions admin

--- backend/mete/admin.py
+++ backend/mete/admin.py
@@ -11,7 +11,19 @@ class KeyPairAdmin(admin.ModelAdmin):
     list_display = ['id', 'account', 'public_key', 'created_at', 'updated_at']
     list_display_links = ['id', 'account']
 
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['created_at', 'amount']
+    list_display_links = []
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 # Register model admins
 admin.site.register(models.Account, AccountAdmin)
 admin.site.register(models.KeyPair, KeyPairAdmin)
-
+admin.site.register(models.Transaction, TransactionAdmin)
