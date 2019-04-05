ec999348159598299d520771469a380d223674d9
Matthias Hannig <mail@matthiashannig.de>
2019-04-05 14:41:50 +0200

    use react hooks

--- webshit/src/components/stats/view.js
+++ webshit/src/components/stats/view.js
@@ -3,49 +3,49 @@
  * StatsView component
  */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchStats } from './actions'
 
-class StatsView  extends React.Component {
+const StatsView = (props) => {
 
-  componentDidMount() {
-    this.props.dispatch(fetchStats());
+  useEffect(() => {
+    props.dispatch(fetchStats());
+  }, []);
 
+  // 
+  useEffect(() => {
     // Refresh stats every huh... 10 seconds or so...
-    this.refreshTimer = setInterval(() => {
-      this.props.dispatch(fetchStats());
+    const refreshTimer = setInterval(() => {
+      props.dispatch(fetchStats());
     }, 10000);
-  }
+  
 
-  componentWillUnmount() {
-    if (this.refreshTimer) {
-      clearInterval(this.refreshTimer);
-    }
-  }
+    return () => {
+      clearInterval(refreshTimer);
+    };
+  }, []);
 
-  render() {
-    return (
-      <table className="stats">
-        <tbody>
-          <tr>
-            <td>TX this month:</td>
-            <td className="value">{this.props.txCurrentMonth}</td>
-            <td>Total:</td>
-            <td className="value">{this.props.txTotal}</td>
-          </tr>
-          <tr>
-            <td>Users:</td><td className="value">{this.props.users}</td>
-          </tr>
-          <tr>
-            <td>Money gauge:</td>
-            <td className="value">{this.props.moneyGauge}</td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>TX this month:</td>
+          <td className="value">{props.txCurrentMonth}</td>
+          <td>Total:</td>
+          <td className="value">{props.txTotal}</td>
+        </tr>
+        <tr>
+          <td>Users:</td><td className="value">{props.users}</td>
+        </tr>
+        <tr>
+          <td>Money gauge:</td>
+          <td className="value">{props.moneyGauge}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default connect(
