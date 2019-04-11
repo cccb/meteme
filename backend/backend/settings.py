ecd5d06c102f34a34955e312ca328b4ccf170690
Matthias Hannig <mail@matthiashannig.de>
2019-04-11 23:53:09 +0200

    allow api calls without csrf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,7 +49,7 @@ ALLOWED_HOSTS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'api.v1.authentication.CsrfExemptSessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
